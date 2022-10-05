c33c4a46716ace85ce172d853940159e9cead05b
PulkitChaudhari <53944886+PulkitChaudhari@users.noreply.github.com>
2022-10-05 21:32:08 +0530

    In these files i completed APIs using Mongoose including Search API with Regex matching

--- index.js
+++ index.js
@@ -390,7 +390,34 @@ app.post("/create",async (req,resp)=>{
     const result = await data.save();
     console.log(result);
 });
+app.get("/list",async (req,resp)=>{
+    let data = await Product.find(); 
+    resp.send(data);
+
+})
+app.delete("/delete/:_id",async (req,resp)=>{
+    let data = await Product.deleteOne(req.params); 
+    resp.send(data);
+})
+app.put("/update/:_id",async (req,resp)=>{
+    let data = await Product.updateOne(
+        req.params,
+        {$set:req.body});
+        resp.send("done");
+})
+app.get("/search/:key",async(req,resp)=>{
+    console.log(req.params.key);
+    let data = await Product.find({
+        "$or":[{"name":{$regex:req.params.key}},
+        {"brand":{$regex:req.params.key}},
+        {"category":{$regex:req.params.key}}
+    ]
+    });
+    console.log(data);
+    resp.send(data);
+})
 app.listen(4500);
+
 
 
 
