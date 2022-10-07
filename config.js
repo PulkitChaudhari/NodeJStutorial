09f8a903ac8d1588d3facb4d28d82599f21efd18
PulkitChaudhari <53944886+PulkitChaudhari@users.noreply.github.com>
2022-10-07 15:13:43 +0530

    In this commit, I completed the series by completing CRUD operations in MySQL using NodeJS

--- config.js
+++ config.js
@@ -1,2 +1,19 @@
+// configuring connection with mongoose
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/e-comm'); 
+
+// configuring connection with mysql
+const mysql = require('mysql');
+const con = mysql.createConnection({
+    host : 'localhost',
+    user : 'root',
+    password : 'pulkit',
+    database : "test"
+});
+con.connect((err)=>{
+    if (err) {
+        console.log("error in connection");
+    }
+    else console.log("all ok");
+})
+module.exports = con;
